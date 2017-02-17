6f85147ceb1fa76768a4e56cc4b0acd55f0ff998
Erica Wright <ericaw21@gmail.com>
2017-02-17 12:19:09 -0800

    meet third spec - pass test to find word once in multiple word string

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,12 +4,16 @@
         function CountRepeats($keyword, $search_string)
         {
             $keyword_letters = str_split($keyword);
-            $search_string_letters = str_split($search_string);
-            if (array_diff($keyword_letters, $search_string_letters) === array()) {
-                return 1;
-            } else {
-                return 0;
+            $search_string_words = explode(" ", $search_string);
+            $total_matches = 0;
+
+            foreach ($search_string_words as $word_to_compare) {
+                $word_letters = str_split($word_to_compare);
+                if (array_diff($keyword_letters, $word_letters) === array()) {
+                    ++$total_matches;
+                }
             }
+            return $total_matches;
         }
 
     }
