4b032b89b7809ba17f59b2194385672c597b10b2
Erica Wright <ericaw21@gmail.com>
2017-02-17 15:26:43 -0800

    add error message if keyword contains spaces or numbers

--- app/app.php
+++ app/app.php
@@ -17,10 +17,31 @@
     $app->post('/result', function() use ($app) {
         $new_word_frequency = new RepeatCounter;
         $keyword = $_POST['keyword'];
+        $space = ' ';
+        $numbers = array ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        $keyword_spacepos = strpos($keyword, $space);
+        $keywords_numbers = array();
+        foreach ($numbers as $number) {
+            $keyword_numberpos = strpos($keyword, $number);
+            if ($keyword_numberpos === false) {
+                $keywords_numbers = array();
+            } else {
+                array_push($keywords_numbers, strpos($keyword, $number));
+            }
+        }
+
+        if ($keyword_spacepos === false) {
+            $error_input = "";
+        } else {
+            $error_input = "Please enter a single word without numbers or spaces";
+        }
+        if (!empty($keywords_numbers)) {
+            $error_input = "Please enter a single word without numbers or spaces";
+        }
         $stringtosearch = $_POST['string_to_search'];
         $result = $new_word_frequency->CountRepeats($keyword, $stringtosearch);
 
-        return $app['twig']->render('result.html.twig', array('result' => $result, 'keyword' => $keyword, 'stringsearch' => $stringtosearch));
+        return $app['twig']->render('result.html.twig', array('result' => $result, 'keyword' => $keyword, 'stringsearch' => $stringtosearch, 'error' => $error_input));
 
     });
 
