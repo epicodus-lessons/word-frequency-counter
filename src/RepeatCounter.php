df5a64eb5820e813b5ced7f4925cee1a35012d6a
Erica Wright <ericaw21@gmail.com>
2017-02-17 12:07:39 -0800

    meet first spec - pass test to match one word to another

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,9 +3,12 @@
     {
         function CountRepeats($keyword, $search_string)
         {
-
+            $keyword_letters = str_split($keyword);
+            $search_string_letters = str_split($search_string);
+            if (array_diff($keyword_letters, $search_string_letters) === array()) {
+                return "1";
+            }
         }
-
 
     }
 
