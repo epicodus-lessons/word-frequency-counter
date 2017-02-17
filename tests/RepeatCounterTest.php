ed3c395e5032bfcc769167f8f15f80e12fefde85
Erica Wright <ericaw21@gmail.com>
2017-02-17 12:25:08 -0800

    create and meet fifth spec - pass test with two matches in multiple word string

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -45,21 +45,35 @@
 
             //Assert
             $this->assertEquals(1, $result);
-          }
+        }
 
-          function test_multiplewordstrings_nomatch()
-          {
-              //Arrange
-              $test_WordFrequency = new RepeatCounter;
-              $word = "cat";
-              $string = "the bat in the hat";
+        function test_multiplewordstrings_nomatch()
+        {
+            //Arrange
+            $test_WordFrequency = new RepeatCounter;
+            $word = "cat";
+            $string = "the bat in the hat";
 
-              //Act
-              $result = $test_WordFrequency->CountRepeats($word, $string);
+            //Act
+            $result = $test_WordFrequency->CountRepeats($word, $string);
 
-              //Assert
-              $this->assertEquals(0, $result);
-          }
+            //Assert
+            $this->assertEquals(0, $result);
+        }
+
+        function test_multiplewordstrings_twomatches()
+        {
+            //Arrange
+            $test_WordFrequency = new RepeatCounter;
+            $word = "cat";
+            $string = "the cat by the cat";
+
+            //Act
+            $result = $test_WordFrequency->CountRepeats($word, $string);
+
+            //Assert
+            $this->assertEquals(2, $result);
+        }
 
     }
 
